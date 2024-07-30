86381f2a94984266d4e329107c4bc5d9160208c8
mattoid <liufei@liufei.info>
2024-07-30 20:31:11 +0800

    取消用户中心 补签卡显示

--- js/src/forum/index.tsx
+++ js/src/forum/index.tsx
@@ -4,9 +4,9 @@ import UserCard from 'flarum/components/UserCard';
 
 app.initializers.add('mattoid/flarum-ext-store-check-in', () => {
 
-  extend(UserCard.prototype, 'infoItems', function (items) {
-    items.add('checkinCard',
-      <span>{app.translator.trans('mattoid-store-check-in.forum.label.checkin-card') + '：' + this.attrs.user.data.attributes['checkinCard']}</span>
-    )
-  })
+  // extend(UserCard.prototype, 'infoItems', function (items) {
+  //   items.add('checkinCard',
+  //     <span>{app.translator.trans('mattoid-store-check-in.forum.label.checkin-card') + '：' + this.attrs.user.data.attributes['checkinCard']}</span>
+  //   )
+  // })
 });
